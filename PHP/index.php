fc3f4da4a65efcc25cce9bc8a1ccdcf4b0f071db
Lucas Silveira <lucas.silveira0516@gmail.com>
2023-10-15 03:17:01 -0300

    adding toggle button to login page

--- PHP/index.php
+++ PHP/index.php
@@ -15,7 +15,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/font.css">
     <title>Index</title>
 </head>
 <body>
